b3b0501c2e6d19f8411ae40efb4d58fdcbc391c9
Leila Elizabeth Gonzalez <layla.elizabeth95@gmail.com>
2022-06-21 09:52:34 -0300

    hidden keys

--- src/services/monitoring.service.ts
+++ src/services/monitoring.service.ts
@@ -33,10 +33,3 @@ export const monitoringService = {
   getPlanMonitoringPlanIdsByPatientId,
   getDevicesIdByPatientID
 };
-
-// Usuario: gf@acuidado.com
-// Clave: Gaby2021
-// 9187c7c0-a84c-4409-8872-371f97e2da47 patient
-// 58e1e451-2cad-48a7-8a53-7ffb3fa53ef7 doctor
-// ba1d4709-7ebf-46c5-98e1-4c4b2c728e68 planid
-// imgDevices/1811b42f-5315-4a5b-b555-affe55294938/chud514-1.jpeg link img
