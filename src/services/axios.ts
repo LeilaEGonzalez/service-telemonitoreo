62e7ec90bbd7d83cf1a0efd11c6ccfddc3ad7956
Leila Elizabeth Gonzalez <86132289+LeilaEGonzalez@users.noreply.github.com>
2022-09-07 11:01:33 -0300

    Update axios.ts

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,27 +1,27 @@
 import axios from 'axios';
 
 export const monitoringPlanRouterInstance = axios.create({
-  baseURL: 'http://localhost:3009'
+  baseURL: 'https://orchestrator-service-development.up.railway.app'
 });
 
 export const authInstance = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://orchestrator-service-development.up.railway.app'
 });
 
 export const monitoringRouterInstance = axios.create({
-  baseURL: 'http://localhost:3008'
+  baseURL: 'https://orchestrator-service-development.up.railway.app'
 });
 
 export const devicesRouterInstance = axios.create({
-  baseURL: 'http://localhost:3005'
+  baseURL: 'https://orchestrator-service-development.up.railway.app'
 });
 
 export const commonsRouterInstance = axios.create({
-  baseURL: 'http://localhost:3009'
+  baseURL: 'https://orchestrator-service-development.up.railway.app'
 });
 
 export const patientInstance = axios.create({
-  baseURL: 'http://localhost:3002'
+  baseURL: 'https://orchestrator-service-development.up.railway.app'
 });
 
 // https://dev.telemonitoreopalcare.com/api/
