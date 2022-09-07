5533c0d2c2ab5558d57119d78352737f1ba4c047
Leila Elizabeth Gonzalez <86132289+LeilaEGonzalez@users.noreply.github.com>
2022-09-07 11:26:32 -0300

    Update axios.ts

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,27 +1,27 @@
 import axios from 'axios';
 
 export const monitoringPlanRouterInstance = axios.create({
-  baseURL: 'https://orchestrator-service-development.up.railway.app'
+  baseURL: 'https://dev.telemonitoreopalcare.com/api/monitoring-plan'
 });
 
 export const authInstance = axios.create({
-  baseURL: 'https://orchestrator-service-development.up.railway.app'
+  baseURL: 'https://dev.telemonitoreopalcare.com/api/auth'
 });
 
 export const monitoringRouterInstance = axios.create({
-  baseURL: 'https://orchestrator-service-development.up.railway.app'
+  baseURL: 'https://dev.telemonitoreopalcare.com/api/monitoring'
 });
 
 export const devicesRouterInstance = axios.create({
-  baseURL: 'https://orchestrator-service-development.up.railway.app'
+  baseURL: 'https://dev.telemonitoreopalcare.com/api/devices'
 });
 
 export const commonsRouterInstance = axios.create({
-  baseURL: 'https://orchestrator-service-development.up.railway.app'
+  baseURL: 'https://dev.telemonitoreopalcare.com/api/commons'
 });
 
 export const patientInstance = axios.create({
-  baseURL: 'https://orchestrator-service-development.up.railway.app'
+  baseURL: 'https://dev.telemonitoreopalcare.com/api/patient'
 });
 
 // https://dev.telemonitoreopalcare.com/api/
